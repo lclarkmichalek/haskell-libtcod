cb56a23da9303c959a72c133149da327a9c040e2
Laurie Clark-Michalek <bluepeppers@archlinux.us>
2012-08-15 18:14:43 +0100

    Fixed problem with TCOD_image_get_mipmap_pixel wrapper

--- UI/TCOD/cimage.c
+++ UI/TCOD/cimage.c
@@ -1,11 +1,11 @@
-#include "ccimage.h"
+#include "cimage.h"
 
 void TCOD_image_get_pixel_ptr(TCOD_image_t img, int x, int y, TCOD_color_t* col) {
     *col = TCOD_image_get_pixel(img, x, y);
 }
 
 void TCOD_image_get_mipmap_ptr(TCOD_image_t img, float x0, float y0, float x1, float y1, TCOD_color_t* col) {
-    col* = TCOD_image_get_mipmap(img, x0, y0, x1, y1);
+    *col = TCOD_image_get_mipmap_pixel(img, x0, y0, x1, y1);
 }
 
 void TCOD_image_clear_ptr(TCOD_image_t img, TCOD_color_t* col) {
